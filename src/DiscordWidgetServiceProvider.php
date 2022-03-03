2e5082e03b71090256f906f8eae1fabc6b3030f4
Syntafin <hello@syntafin.dev>
2022-03-03 18:57:06 +0100

    View Composer to Include CSS if TailwindCSS is not used in the project (default true)

--- src/DiscordWidgetServiceProvider.php
+++ src/DiscordWidgetServiceProvider.php
@@ -10,6 +10,12 @@ class DiscordWidgetServiceProvider extends ServiceProvider {
     public function boot() {
         Livewire::component('discord-widget', Widget::class);
 
+        View::composer('discord-widget', function ($view) {
+            if(config('discord-widget.include_css')) {
+                $view->cssPath = __DIR__ . '/../resources/css/discord-discord-widget.css';
+            }
+        });
+
         $this->loadViewsFrom(__DIR__.'/views', 'discord-widget');
         $this->loadTranslationsFrom(__DIR__.'/lang', 'discord-widget');
         $this->mergeConfigFrom(__DIR__.'/config.php', 'discord-widget');
--- src/config.php
+++ src/config.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'avatar' => env('DISCORD_AVATAR', true)
+    'avatar' => env('DISCORD_AVATAR', true),
+
+    /**
+     * If set to true, the included CSS is used,
+     * you can set to false if you use TailwindCSS in your project.
+     *
+     */
+    'include_css' => 'true',
 ];
