59020baec1d08c7414b5e1e4f74b26b2eca12e0c
Syntafin <hello@syntafin.dev>
2022-03-05 03:56:50 +0100

    Add Simplebar Data-Attribute

--- src/views/widget.blade.php
+++ src/views/widget.blade.php
@@ -54,7 +54,7 @@
                 {{ $data->member_count }}
             </span>
         </p>
-        <div class="h-64 overflow-y-auto">
+        <div class="h-64 overflow-y-auto" data-simplebar>
             <ul class="divide-y divide-gray-200">
                 @foreach($data->member_list as $member)
                     @php
