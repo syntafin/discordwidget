b6bb443900d022738b5328834ca8292741907aff
Syntafin <hello@syntafin.dev>
2022-08-13 22:46:23 +0200

    Button Size and Styling

--- src/views/widget.blade.php
+++ src/views/widget.blade.php
@@ -100,7 +100,7 @@
             </div>
         </div>
     </div>
-    <a href="{{ $data->instant_invite }}" class="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded w-full shadow-sm text-white {{ $button }} {{ $buttonHover }} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-auto">
+    <a href="{{ $data->instant_invite }}" class="inline-flex items-center px-5 py-2 border border-transparent font-medium rounded w-full shadow-sm text-white {{ $button }} {{ $buttonHover }} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-auto">
         @lang('discord-widget::widget.invite')
     </a>
     @endisset
